374a99d32fd620f80a30fc4f9eeb87060c882c71
Alexander Wunschik <alex@wunschik.net>
2011-11-20 12:52:39 +0100

    initial commit

---
+++ TCP2000.pm
@@ -0,0 +1,276 @@
+package TCP2000;
+$VERSION = '0.1';
+
+use Printer;
+
+my $STYLE_NONE = 0x0000;
+my $STYLE_BOLD = 0x0001;
+my $STYLE_UNDERLINED = 0x0002;
+my $STYLE_OVERLINED = 0x0004;
+my $STYLE_INVERTED = 0x0008;
+
+my $ALIGN_LEFT = 0x30;
+my $ALIGN_CENTER = 0x31;
+my $ALIGN_RIGHT = 0x32;
+
+my $LINE_LENGTH = 44;
+
+###################################################################
+
+sub error {
+	my $msg = shift;
+	print $msg;
+}
+
+sub trace {
+	my $msg = shift;
+	#print $msg;
+}
+
+############################################################################
+my $prn;
+my $sendBuffer;
+
+$prn = new Printer( 'linux' => 'lpr' );
+$prn->print_command(
+	'linux' => {
+		'type'    => 'pipe',
+		'command' => 'lpr'
+	}
+);
+#my %available_printers = $prn->list_printers;
+#$prn->use_default;
+
+############################################################################
+sub new {
+	# constructor
+	my $type = shift;
+	my %params = @_;
+	my $self = {};
+	return bless $self, $type;
+}
+
+############################################################################
+sub clean_buffer {
+	$sendBuffer = "";
+}
+
+############################################################################
+sub print {
+	my ($self, $data) = @_;
+	$sendBuffer = $sendBuffer.$data;
+}
+
+############################################################################
+sub print_text {
+	my ($self, $data) = @_;
+	my $line = "";
+	
+	$data =~ s/\n/\n\xE8/g;
+	@words =  split(/\s/, $data);
+	
+	foreach (@words) {
+		my $word = $_;
+		$word =~ s/\xE8/\x0A/g;
+		
+		trace "WORD: ".$word."\n";
+		trace "LENGTH: ".length($line." ".$word)."\n";
+		
+		# Auf Zeilenlänge von 44+LF beschneiden. Bei Ist ein LF enthalten dann neue Zeile beginnen
+		if ($word eq "\n") {
+			$sendBuffer = $sendBuffer.$line.chr(0x0A);
+			$line = "";
+		} elsif (length($line.$word." ") <= $LINE_LENGTH) {
+			$line = $line.$word." ";
+		} elsif (length($line.$word) == $LINE_LENGTH) {
+			$line = $line.$word;
+			$sendBuffer = $sendBuffer.$line;
+			$line = "";
+		} else {
+			$sendBuffer = $sendBuffer.$line.chr(0x0A);
+			$line = $word." ";
+		}	
+		trace "LINE: ".$line."\n------------------------------------------------\n";
+	}
+	$sendBuffer = $sendBuffer.$line."\n";
+}
+
+############################################################################
+sub init {
+	my ($self) = @_;
+	#reset printer buffer
+	$self->print( chr(0x1B).chr(0x40) );
+}
+	
+############################################################################
+sub set_chartable {
+	my ($self, $arg) = @_;
+	#TODO wertebereich abfragen 0x00-0x0A, 0x30-0x39 & 0x41
+	$self->print( chr(0x1B).chr(0x1D).chr(0x74).$arg );
+}
+
+############################################################################
+sub set_charset {
+	my ($self, $arg) = @_;
+	#TODO wertebereich abfragen 0x00-0x0C, 0x30-0x39 & 0x41-0x43
+	$self->print( chr(0x1B).chr(0x52).$arg );
+}
+
+############################################################################
+sub set_char_spacing {
+	my ($self,$arg) = @_;
+	#TODO Wertebereich abfragen 0x01-0x0F, 0x31-0x39 & 0x41-0x46 
+	$self->print( chr(0x1B).chr(0x20).$arg );
+}
+
+############################################################################
+sub set_line_spacing {
+	my ($self, $arg) = @_;
+	#TODO Wertebereich abfragen 3-4
+	if ($arg eq "3mm") {
+		$self->print( chr(0x1B).chr(0x30) );
+	} elsif ($arg eq "4mm") {
+		$self->print( chr(0x1B).chr(0x7A).chr(0x01) );
+	} else {
+		error("line spacing %arg is not allowed. Please choose between \"3mm\" or \"4mm\".\n");
+	}
+}
+
+############################################################################
+sub set_style {
+	my ($self, $style) = @_;
+		
+	trace "Style: ";
+	
+	if ($style & $STYLE_BOLD) {
+		trace "<BOLD> ";
+		$self->print( chr(0x1B).chr(0x45) );
+	} else {
+		trace "BOLD ";
+		$self->print( chr(0x1B).chr(0x46) );
+	}
+	
+	if ($style & $STYLE_UNDERLINED) {
+		trace "<UNDERLINED> ";
+		$self->print( chr(0x1B).chr(0x2D).chr(0x01) );
+	} else {
+		trace "UNDERLINED ";
+		$self->print( chr(0x1B).chr(0x2D).chr(0x00) );
+	}
+	
+	if ($style & $STYLE_OVERLINED) {
+		trace "<OVERLINED> ";
+		$self->print( chr(0x1B).chr(0x5F).chr(0x01) );
+	} else {
+		trace "OVERLINED ";
+		$self->print( chr(0x1B).chr(0x5F).chr(0x00) );
+	}
+	
+	if ($style & $STYLE_INVERTED) {
+		trace "<INVERTED> ";
+		$self->print( chr(0x1B).chr(0x34) )
+	} else {
+		trace "INVERTED ";
+		$self->print( chr(0x1B).chr(0x35) );
+	}
+	
+	print "\n";
+}
+
+############################################################################
+sub set_char_width {
+	my ($self, $arg) = @_;
+	#TODO wertebereich überprüfen 0x00-0x05, 0x30-0x35
+	$self->print( chr(0x1B).chr(0x57).$arg );
+}
+
+############################################################################
+sub set_char_hight {
+	my ($self, $arg) = @_;
+	#TODO wertebereich überprüfen 0x00-0x05, 0x30-0x35
+	$self->print( chr(0x1B).chr(0x68).$arg );
+}
+
+############################################################################
+sub set_text_size {
+	my ($self, $size) = @_;
+	$self->set_char_width($size);
+	$self->set_char_hight($size); 
+}
+
+############################################################################
+sub set_alignment {
+	my ($self, $alignment) = @_;
+	#TODO Wertebereich überprüfen
+	if ($alignment == $ALIGN_LEFT) {
+		trace "Alignment: LEFT\n";
+		$self->print( chr(0x1B).chr(0x1D).chr(0x61).chr(0x30) );
+	} elsif ($alignment == $ALIGN_CENTER) {
+		trace "Alignment: CENTER\n";
+		$self->print( chr(0x1B).chr(0x1D).chr(0x61).chr(0x31) );
+	} elsif ($alignment == $ALIGN_RIGHT) {
+		trace "Alignment: RIGHT\n";
+		$self->print( chr(0x1B).chr(0x1D).chr(0x61).chr(0x32) );
+	} else {
+		error("invalid alignment $alignment");
+	}
+}
+
+############################################################################
+sub cut_paper {
+	my ($self) = @_;
+	$self->print( chr(0x0C) );
+}
+
+############################################################################
+sub print_testpage {
+	my ($self) = @_;
+	$self->print( chr(0x1B).chr(0x3F).chr(0x0A).chr(0x00) );
+}
+
+############################################################################
+sub print_line {
+	my ($self) = @_;
+	
+	for (my $i = 0; $i < $LINE_LENGTH+4; $i++ ) {
+		$self->print( "_" );
+	}
+}
+
+############################################################################
+sub reset_style {
+	my ($self) = @_;
+	
+	$self->set_style( $STYLE_NONE );
+	$self->set_alignment($ALIGN_LEFT);
+	$self->set_text_size(0);
+	$self->set_char_spacing(1);
+	$self->set_line_spacing("4mm");
+	
+   	$self->print("x123456789x123456789x123456789x123456789x123\n");
+   #$self->print("____________________________________________\n");
+	$self->print("--------------------------------------------\n");
+}
+
+############################################################################
+sub set_mirrored {
+	my ($self, $arg) = @_;
+
+	#TODO wertebereich festlegen und überprüfen
+	if ($arg == 1) {
+		$self->print( chr(0x0F) );
+	} else {
+		$self->print( chr(0x12) );
+	}
+}
+
+############################################################################
+sub flush {
+	my ($self) = @_;
+	$prn->print( $sendBuffer );
+}
+
+############################################################################
+__END__
+
+# documentation
---
+++ twitter.pl
@@ -0,0 +1,231 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+use XML::RSS;
+use XML::RSS::TimingBot;
+use XML::RSS::Parser::Lite;
+use LWP::Simple;
+use URI::Escape;
+
+use lib "/home/twinter/POSprinter";
+use TCP2000;
+
+my $STYLE_NONE = 0x0000;
+my $STYLE_BOLD = 0x0001;
+my $STYLE_UNDERLINED = 0x0002;
+my $STYLE_OVERLINED = 0x0004;
+my $STYLE_INVERTED = 0x0008;
+
+my $ALIGN_LEFT = 0x30;
+my $ALIGN_CENTER = 0x31;
+my $ALIGN_RIGHT = 0x32;
+
+my $tcp2k = new TCP2000();
+
+#$ENV{'TIMINGBOTPATH'} = '.';
+
+#my $hash = "%23CCCamp11"; 
+my $hash = "%23Piraten";
+#my $hash = "wunschik";
+
+###### CHANGE url.txt  !!!!!!!!!
+my $url = qq("http://search.twitter.com/search.rss?q=$hash&result_type=recent&show_user=true");
+#my $url = 'http://twitter.com/statuses/user_timeline/16351337.rss';
+#my $url = 'http://www.soup.io/notifications/ca309cbd4f2d4657f4b6eb0b60221c7e.rss';
+
+#----------------------------------------------------------------
+sub init_printer {
+	$tcp2k->init();
+	$tcp2k->set_chartable(0);
+	$tcp2k->set_charset(0);
+	$tcp2k->set_char_spacing(1);
+	$tcp2k->set_line_spacing("4mm");
+}
+
+#----------------------------------------------------------------
+sub clean_text {
+        my $text = shift;
+	$text =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+
+        # Whitespaces am Anfang und Ende wegfressen
+        $text =~ s/^\s*//g;
+        $text =~ s/\s*$//g;
+
+	$text =~ s/\n//ig;
+	
+        # HTML-Zeilenumbruch behandeln
+        $text =~ s/&lt;br&gt;/\n/g;
+
+        # Style: Bold
+        $text =~ s/&lt;b&gt;/\x1B\x45/g;
+        $text =~ s/&lt;\/b&gt;/\x1B\x46/g;
+
+	# highlight mentions
+	$text =~ s/(@.*? )/\x1B\x45$1\x1B\x46/g;
+	
+	# author
+	$text =~ s/^(.*?): /\x1B\x45\x1B\x57\x01$1\n\x1B\x57\x00\x1B\x46/g;
+	
+	$text =~ s/(http:\/\/.*? )/\x1B\x2D\x01$1\x1B\x2D\x00/g;
+	$text =~ s/(http:\/\/.*?)$/\x1B\x2D\x01$1\x1B\x2D\x00/g;
+
+	$text =~ s/&lt;\/em&gt//ig;
+
+        #HTML Links
+        $text =~ s/ class=".*?"//ig;
+        $text =~ s/ title=".*?"//ig;
+        $text =~ s/&lt;a href="(.*?)"&gt;(.*?)&lt;\/a&gt;/$2/ig;
+        #$text =~ s/&lt;a href="(.*?)" class=".*?" &gt;(.*?)&lt;\/a&gt;/$1/ig;
+        #$text =~ s/&lt;a href="(.*?)" title=".*?" class=".*?" \/a&gt(.*?)&lt;/$2/ig;
+
+        #Alle anderen HTML Tags entfernen
+        $text =~ s/&lt;.*&gt;//g;
+
+        # Sonderzeichen umwandeln
+        $text =~ s/&amp;#34;/\"/g; # Anführungszeichen
+        $text =~ s/&amp;#39;/\'/g; # Aprostroph
+        $text =~ s/´/\'/g; # Aprostroph
+
+        # Style: Bold
+        $text =~ s/&lt;b&gt;/\x1B\x45/g;
+        $text =~ s/&lt;\/b&gt;/\x1B\x46/g;
+
+        #Alle anderen HTML Tags entfernen
+        #$text =~ s/&lt;.*&gt;//g;
+	$text =~ s/&lt;em&gt;//g;
+
+        # Sonderzeichen umwandeln
+        $text =~ s/&amp;#34;/\"/g; # Anführungszeichen
+        $text =~ s/&amp;#39;/\'/g; # Aprostroph
+        $text =~ s/´/\'/g; # Aprostroph
+
+        # Deutsche Sonderzeichen        
+        $text =~ s/&#xe4;/\xCD/g; #ä
+        $text =~ s/Ä/\xA0/g;
+        $text =~ s/ö/\xB9/g;
+        $text =~ s/Ö/\xA1/g;
+        $text =~ s/&#xfc;/\xBE/g; #ü
+        $text =~ s/Ü/\xA2/g;
+        $text =~ s/ß/\xA3/g;
+        $text =~ s/\?/\x3F/g;
+
+        return $text;
+}
+
+#----------------------------------------------------------------
+sub  loop { 
+	open FILE, "<url.txt";
+	my @lines = <FILE>;
+	my $oldUrl = $lines[0];
+	if ( length($oldUrl) > 0 ) { $url = $oldUrl };
+	close FILE;
+
+	my $data = get( $url );
+	my $rss = XML::RSS->new();
+
+	if (!$data) { die "data could not be read" };
+
+	#stupid XML::RSS can not handle tags with nontext charecters
+	$data =~ s/twitter:refresh_url/twitter_refresh_url/ig;
+	$data =~ s/openSearch:itemsPerPage/openSearch_itemsPerPage/ig;
+
+	$rss->parse( $data );
+	
+	#print $data."\n\n";
+
+	my $channel = $rss->{channel};
+
+	my $itemcount = $channel->{openSearch_itemsPerPage};
+	if (!$itemcount) {
+		print "no new tweets yet :-(\n";
+		goto NO_TWEETS;
+	}
+
+	$url = $channel->{twitter_refresh_url};
+	$url =~ s/&amp;/&/ig;
+	$url = $url."&rpp=15&result_type=recent&show_user=true";
+
+	print("Channel: ".$channel->{title}."\n");
+        print("url: ".$url."\n");
+	print("pubDate: ".$channel->{pubDate}."\n");
+	print("itemcount: ".$itemcount."\n");
+
+	$tcp2k->clean_buffer();
+
+	if (rand(10)<1) {
+		$tcp2k->set_text_size(3);
+		$tcp2k->set_style( $STYLE_BOLD );
+		$tcp2k->print_text( $hash );	
+
+		$tcp2k->set_text_size(0);
+		$tcp2k->set_char_width(0);
+		$tcp2k->set_style( $STYLE_NONE );
+		$tcp2k->print_text( "hacked together in perl by" );
+		$tcp2k->set_style( $STYLE_BOLD );
+		$tcp2k->print_text( 'delphiN, @wunschik, delphiN.soup.io' ); 
+		
+		#$tcp2k->set_char_spacing(7);
+		#$tcp2k->set_style( $STYLE_UNDERLINED );
+		#$tcp2k->print_text( "http://twinter.wunschik.it\n" );
+		#$tcp2k->print_line( );	
+		
+		$tcp2k->print_line( );
+		#$tcp2k->print_text( "\n" );
+	}	
+
+	$tcp2k->set_text_size(0);
+	$tcp2k->set_char_width(0);
+	$tcp2k->set_style( $STYLE_NONE );
+
+	$tcp2k->set_char_width(0);
+	$tcp2k->set_text_size(0);
+	$tcp2k->set_char_spacing(0);
+        $tcp2k->set_style( $STYLE_NONE );
+        $tcp2k->set_alignment($ALIGN_CENTER);
+
+
+	for my $i (@{$rss->{items}}) {
+		my $item = $i;		
+		
+		my $text = $item->{title};
+		my $pubDate = $item->{pubDate};
+		#$pubDate =~ s/(.*)\+.*/$1/ig;
+		#$pubDate =~ s/\s?, \d? \s? (\d?):(\d?):(\d?) /$1:$2:$3/ig;
+		my $author = $item->{author};	
+		$author =~ s/^.*\((.*)\)/$1/ig;
+
+		$text = clean_text($text);
+
+		print "\n..............................................................................................\n";		
+		print "PUBDATE: ".$pubDate."\n";
+		print "AUTHOR: ".$author."\n";
+		print "TITLE: ".$text."\n";
+
+		$tcp2k->print_text($pubDate);		
+		$tcp2k->print_text( $text );
+
+		$tcp2k->set_char_width(0);
+		$tcp2k->set_text_size(0);
+		$tcp2k->set_char_spacing(0);
+		$tcp2k->set_style( $STYLE_NONE );
+		$tcp2k->set_alignment($ALIGN_CENTER);
+		$tcp2k->print_line( );
+		#$tcp2k->cut_paper();
+	}
+	$tcp2k->flush();
+
+	open FILE, ">url.txt";
+	print FILE $url;
+	close FILE;
+
+NO_TWEETS:
+	#no new tweets
+}
+
+init_printer();
+while (1) {
+	loop();
+	sleep(60);
+}
